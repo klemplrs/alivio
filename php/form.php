f97fc280655c0a26a33e3eb8699cd93b6debcdb0
klemplrs <clementperso.dp@gmail.com>
2021-03-14 23:33:39 +0100

    css/ responsive for other devices

--- php/form.php
+++ php/form.php
@@ -1,14 +1,18 @@
 <?php
 
-if (isset($_POST['message'])) {
-    $position_arobase = strpos($_POST['email'], '@');
-    if ($position_arobase === false)
-        echo '<p>Votre email doit comporter un arobase.</p>';
-    else {
-        $retour = mail('clementperso.dp@gmail.com', 'Envoi depuis le formulaire de Contact', $_POST['message'], 'From: ' . $_POST['email']);
-        if ($retour)
-            echo '<p>Votre message a été envoyé.</p>';
-        else
-            echo '<p>Erreur.</p>';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    if (isset($_POST['message'])) {
+        $position_arobase = strpos($_POST['email'], '@');
+        if ($position_arobase === false)
+            echo '<p>Votre email doit comporter un arobase.</p>';
+        else {
+            $retour = mail('clementperso.dp@gmail.com', 'Envoi depuis le formulaire de Contact', $_POST['message'], 'From: ' . $_POST['email']);
+            if ($retour)
+                echo '<p>Votre message a été envoyé.</p>';
+            else
+                echo '<p>Erreur.</p>';
+        }
     }
+
 }
